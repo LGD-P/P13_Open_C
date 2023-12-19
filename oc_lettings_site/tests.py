1fe2615b0a5b6852c59d7e387b4a89bb27c0bfed
LGD-P <nplgdjp@gmx.com>
2023-12-20 00:50:31 +0100

    tests.py

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,12 @@
-def test_dummy():
-    assert 1
+from django.test import Client
+from django.urls import reverse
+
+
+def test_index():
+    client = Client()
+    response = client.get(reverse('index'))
+    assert response.request['PATH_INFO'] == '/'
+    assert response.status_code == 200
+    assert 'Welcome to Holiday Homes' in response.content.decode()
+    assert 'Profiles' in response.content.decode()
+    assert 'Lettings' in response.content.decode()
