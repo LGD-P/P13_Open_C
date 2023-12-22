11865ef7f31369b8d0142903f2c2f3463579147c
LGD-P <nplgdjp@gmx.com>
2023-12-22 04:00:44 +0100

    add logger in urls

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+import logging
 
 from . import views
+
+logger = logging.getLogger(__name__)
+
+
+def trigger_error(request):
+    logger.info('info')
+    logger.error('error')
+    division_by_zero = 1 / 0
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error, name='sentry'),
 ]
