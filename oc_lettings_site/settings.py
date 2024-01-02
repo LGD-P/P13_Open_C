75c1feaa4eba643f1a9980daa83b3e8b59ed9cd5
LGD-P <nplgdjp@gmx.com>
2024-01-02 23:21:40 +0100

    settings secret key

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SETTINGS_SECRET_KEY')
 
+
 # SECURITY WA
 # RUNNING: don't run with debug turned on in production!
 DEBUG = True
