7ebaf959fe56ae88e1de4d61e17a25ff5276ab8d
LGD-P <nplgdjp@gmx.com>
2024-01-15 14:54:20 +0100

    new class profiles docs

--- profiles/models.py
+++ profiles/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     """
-    This Class defines a Django model for representing a user's profile.
+    Class model represent a user’s profile.
 
     Classes:
         - Profile: A model representing a user's profile.
